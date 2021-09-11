e88613c7452a54eb581e166fd7f698e0eb183c48
txttaini <87921207+txttaini@users.noreply.github.com>
2021-09-12 01:15:49 +0800

    Update system.js

--- system.js
+++ system.js
@@ -63,23 +63,23 @@ setTimeout(e, 1e3 / 60)
 i(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"),
 n(),
 r() } (window, document);
-//屏蔽
-    var system = {
-        win: false,
-        mac: false,
-        xll: false
-    };
-    var p = navigator.platform;
-    var us = navigator.userAgent.toLowerCase();
-    system.win = p.indexOf("Win") == 0;
-    system.mac = p.indexOf("Mac") == 0;
-    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-    if (system.win || system.mac || system.xll) {
-    var iframe_url=cms.root+'stop.html';
-    $("head").html('<meta charset="UTF-8"><meta name="referrer" content="no-referrer"><title>抱歉，站点已暂停</title><style>body{position:static !important;}body *{ visibility:hidden; }</style> ');
-    $("body").empty();
-    $(document).ready(function () {
-    $("body").html('<iframe style="width:100%; height:460px;position:absolute;margin-left:0px;margin-top:0px;left:0%;" id="mainFrame" src="'+iframe_url+'" frameborder="0" scrolling="no"></iframe>').show();
-    $("body *").css("visibility", "visible");
-  });
-}
+// //屏蔽
+//     var system = {
+//         win: false,
+//         mac: false,
+//         xll: false
+//     };
+//     var p = navigator.platform;
+//     var us = navigator.userAgent.toLowerCase();
+//     system.win = p.indexOf("Win") == 0;
+//     system.mac = p.indexOf("Mac") == 0;
+//     system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+//     if (system.win || system.mac || system.xll) {
+//     var iframe_url=cms.root+'stop.html';
+//     $("head").html('<meta charset="UTF-8"><meta name="referrer" content="no-referrer"><title>抱歉，站点已暂停</title><style>body{position:static !important;}body *{ visibility:hidden; }</style> ');
+//     $("body").empty();
+//     $(document).ready(function () {
+//     $("body").html('<iframe style="width:100%; height:460px;position:absolute;margin-left:0px;margin-top:0px;left:0%;" id="mainFrame" src="'+iframe_url+'" frameborder="0" scrolling="no"></iframe>').show();
+//     $("body *").css("visibility", "visible");
+//   });
+// }
